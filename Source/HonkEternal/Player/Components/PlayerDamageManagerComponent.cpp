e4c034f11de29f5a9decd7daed2aac4fa0c4a804
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-10-08 23:05:09 +0300

    Create player damage component

---
+++ Source/HonkEternal/Player/Components/PlayerDamageManagerComponent.cpp
@@ -0,0 +1,57 @@
+﻿// Made dy Titled Goose Team
+
+
+#include "PlayerDamageManagerComponent.h"
+
+#include "PlayerArmorComponent.h"
+#include "HonkEternal/Components/HealthComponent.h"
+
+
+UPlayerDamageManagerComponent::UPlayerDamageManagerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void UPlayerDamageManagerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	HealthComponent = GetOwner()->FindComponentByClass<UHealthComponent>();
+	ArmorComponent = GetOwner()->FindComponentByClass<UPlayerArmorComponent>();
+
+	if (ensureMsgf(HealthComponent && ArmorComponent,
+	               TEXT("PlayerDamageManager component can't find Health and/or Armor components in %s"),
+	               *GetOwner()->GetName()))
+	{
+		GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UPlayerDamageManagerComponent::HandleOnAnyDamageTaken);
+	}
+}
+
+void UPlayerDamageManagerComponent::ApplyDamage(const int32 DamageAmount) const
+{
+	if (!HealthComponent || !ArmorComponent)
+	{
+		return;
+	}
+
+	int32 CurrentDamage = DamageAmount;
+
+	if (ArmorComponent->GetCurValue() > 0)
+	{
+		CurrentDamage -= DamageAmount * ArmorDamageModifier;
+		ArmorComponent->DecreaseValue(DamageAmount * ArmorDamageModifier);
+	}
+
+	HealthComponent->DecreaseValue(CurrentDamage);
+}
+
+void UPlayerDamageManagerComponent::HandleOnAnyDamageTaken(AActor* DamagedActor,
+                                                           float Damage,
+                                                           const UDamageType* DamageType,
+                                                           AController* InstigatedBy,
+                                                           AActor* DamageCauser)
+{
+	Damage *= GlobalDamageModifier;
+	ApplyDamage(FMath::CeilToInt32(Damage));
+}
---
+++ Source/HonkEternal/Player/Components/PlayerDamageManagerComponent.h
@@ -0,0 +1,44 @@
+﻿// Made dy Titled Goose Team
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PlayerDamageManagerComponent.generated.h"
+
+class UHealthComponent;
+class UPlayerArmorComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class HONKETERNAL_API UPlayerDamageManagerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPlayerDamageManagerComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY()
+	TObjectPtr<UHealthComponent> HealthComponent = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<UPlayerArmorComponent> ArmorComponent = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, meta=(ClampMin=0))
+	double GlobalDamageModifier = 1.f;
+
+	UPROPERTY(EditDefaultsOnly, meta=(ClampMin=0))
+	double ArmorDamageModifier = 0.3333333f;
+
+	void ApplyDamage(const int32 DamageAmount) const;
+
+	UFUNCTION()
+	void HandleOnAnyDamageTaken(AActor* DamagedActor,
+	                            float Damage,
+	                            const class UDamageType* DamageType,
+	                            class AController* InstigatedBy,
+	                            AActor* DamageCauser);
+};
