ddd38b80b5e0860b6463667fcb16551822a59521
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-10-08 13:27:28 +0300

    Create a player base class

---
+++ Source/HonkEternal/Player/PlayerCharacter.cpp
@@ -0,0 +1,26 @@
+﻿// Made dy Titled Goose Team
+
+#include "PlayerCharacter.h"
+
+
+APlayerCharacter::APlayerCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/HonkEternal/Player/PlayerCharacter.h
@@ -0,0 +1,24 @@
+﻿// Made dy Titled Goose Team
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class HONKETERNAL_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	APlayerCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
