1caa63ca83dfcd5bbb388a65aa22a3faada3f07e
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-10-08 22:38:25 +0300

    Create a health component

---
+++ Source/HonkEternal/Components/HealthComponent.cpp
@@ -0,0 +1,5 @@
+﻿// Made dy Titled Goose Team
+
+
+#include "HealthComponent.h"
+
---
+++ Source/HonkEternal/Components/HealthComponent.h
@@ -0,0 +1,15 @@
+﻿// Made dy Titled Goose Team
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseEntityResource.h"
+#include "HealthComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class HONKETERNAL_API UHealthComponent : public UBaseEntityResource
+{
+	GENERATED_BODY()
+
+};
