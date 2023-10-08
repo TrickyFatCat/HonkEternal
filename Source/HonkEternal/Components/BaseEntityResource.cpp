7156515b952690a1ef7aa700248eb229f435ae0c
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-10-08 22:36:27 +0300

    Create base entity resource component

---
+++ Source/HonkEternal/Components/BaseEntityResource.cpp
@@ -0,0 +1,60 @@
+﻿// Made dy Titled Goose Team
+
+
+#include "BaseEntityResource.h"
+
+
+UBaseEntityResource::UBaseEntityResource()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void UBaseEntityResource::BeginPlay()
+{
+	CurValue = MaxValue;
+	Super::BeginPlay();
+}
+
+double UBaseEntityResource::GetRelativeCurValue() const
+{
+	const double Result = MaxValue <= 0 ? 0.f : static_cast<double>(CurValue) / static_cast<double>(MaxValue);
+	return Result;
+}
+
+bool UBaseEntityResource::IncreaseValue(const int32 Amount, const bool bClampToMax)
+{
+	if (Amount <= 0 || (bClampToMax && CurValue >= MaxValue))
+	{
+		return false;
+	}
+
+	CurValue += Amount;
+	CurValue = bClampToMax ? FMath::Min(CurValue, MaxValue) : CurValue;
+	
+	OnValueChanged.Broadcast();
+	OnValueIncreased.Broadcast(CurValue, MaxValue, GetRelativeCurValue(), Amount);
+
+	return true;
+}
+
+bool UBaseEntityResource::DecreaseValue(const int32 Amount)
+{
+	if (Amount <= 0 || CurValue <= 0)
+	{
+		return false;
+	}
+
+	CurValue -= Amount;
+	CurValue = FMath::Max(CurValue, 0);
+
+	OnValueChanged.Broadcast();
+	OnValueDecreased.Broadcast(CurValue, MaxValue, GetRelativeCurValue(), Amount);
+
+	if (CurValue <= 0)
+	{
+		OnValueZero.Broadcast();
+	}
+	
+	return true;
+}
---
+++ Source/HonkEternal/Components/BaseEntityResource.h
@@ -0,0 +1,70 @@
+﻿// Made dy Titled Goose Team
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "BaseEntityResource.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnValueChangedSignature);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnValueZeroSignature);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FOnValueIncreasedSignature,
+                                              int32, CurValue,
+                                              int32, MaxValue,
+                                              double, RelativeCurValue,
+                                              int32, DeltaValue);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FOnValueDecreasedSignature,
+                                              int32, CurValue,
+                                              int32, MaxValue,
+                                              double, RelativeCurValue,
+                                              int32, DeltaValue);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class HONKETERNAL_API UBaseEntityResource : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UBaseEntityResource();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnValueChangedSignature OnValueChanged;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnValueZeroSignature OnValueZero;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnValueIncreasedSignature OnValueIncreased;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnValueDecreasedSignature OnValueDecreased;
+	
+	UFUNCTION(BlueprintGetter)
+	int32 GetCurValue() const { return CurValue; }
+
+	UFUNCTION(BlueprintGetter)
+	int32 GetMaxValue() const { return MaxValue; }
+
+	UFUNCTION(BlueprintPure)
+	double GetRelativeCurValue() const;
+
+	UFUNCTION(BlueprintCallable)
+	bool IncreaseValue(const int32 Amount, const bool bClampToMax = true);
+
+	UFUNCTION(BlueprintCallable)
+	bool DecreaseValue(const int32 Amount);
+
+protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetCurValue)
+	int32 CurValue = 100;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter=GetMaxValue, meta=(ClampMin=0))
+	int32 MaxValue = 100;
+};
